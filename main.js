c4367267905b008270e494c11a1668e3096acdb0
Anmol Khanna <anmolkhanna36@gmail.com>
2020-05-22 19:16:24 -0600

    set up P5.js for use with Pointer Events

--- main.js
+++ main.js
@@ -1,29 +1,16 @@
-var WILL = {
-    backgroundColor: Module.Color.BLUE,
-    color: Module.Color.from(255, 204, 204),
+function setup(){
+    var canvasWidth = windowWidth*0.8;
+    var canvasHeight = windowHeight*0.8;
+    createCanvas(canvasWidth,canvasHeight, P2D);
+    background(200,0,0);
+    frameRate(200);
+}
 
-    init: function(width, height) {
-        this.canvas = new Module.InkCanvas(document.getElementById("canvas"), width, height);
-        this.canvas.clear(this.backgroundColor);
-        this.brush = new Module.DirectBrush();
-        this.pathBuilder = new Module.SpeedPathBuilder();
-        this.pathBuilder.setNormalizationConfig(182, 3547);
-        this.pathBuilder.setPropertyConfig(Module.PropertyName.Width, 2.05, 34.53, 0.72, NaN, Module.PropertyFunction.Power, 1.19, false);
-
-        this.strokeRenderer = new Module.StrokeRenderer(this.canvas, this.canvas);
-        this.strokeRenderer.configure({brush: this.brush, color: this.color});
-    },
-
-    draw: function() {
-        var points = [0,300,10, 100,100,20, 400,100,40, 500,300,50];
-        var path = this.pathBuilder.createPath(points);
-
-        this.strokeRenderer.draw(path, true);
+function draw(){
+    brushRadius = 10;
+    if(mouseIsPressed){
+        fill(0);
+        ellipse(mouseX, mouseY, brushRadius, brushRadius);
     }
-};
-
-Module.addPostScript(function() {
-    WILL.init(1600, 600);
-    WILL.draw();
-});
-
+    
+}
