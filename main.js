d6cc963846880d7b4b61c24cbe67bff7561509cc
Anmol Khanna <anmolkhanna36@gmail.com>
2020-05-23 01:12:16 -0600

    Need to improve pen experience

--- main.js
+++ main.js
@@ -4,26 +4,45 @@ function setup(){
     createCanvas(canvasWidth,canvasHeight, P2D);
     background(200,0,0);
     frameRate(200);
+    
+    noLoop();
 }
 
 function draw(){
-    brushRadius = 10;
     /*if(mouseIsPressed){
         fill(0);
         ellipse(mouseX, mouseY, brushRadius, brushRadius);
     }*/
-    //var downHandler = window.onpointerdown; //downHandler is the event
+    var brushRadius = 10;
+    var bCanPaint = false;
     var windowDownHandler = window.onpointerdown; //downHandler is the event
     var cnv = document.getElementById("defaultCanvas0");
     if(cnv){
-        
         cnv.onpointerdown = handlePointerDown;
-        function handlePointerDown(event){
-            console.log("Pointer type is "+ event.pointerType);
-        }
+        cnv.onpointerup = handlePointerUp;
+        cnv.onpointermove = handlePointerMove;
+    }
+    if(cnv.onpointerdown && cnv.onpointermove){
         
     }
-    
+    function handlePointerDown(event){ // sets pressure
+        //console.log("Pointer type is "+ event.pointerType);
+        //console.log("Pointer pressure is "+ event.pressure);
+        brushRadius = event.pressure*50;
+        bCanPaint = true;
+        
+        //fill(0); // fill() only works for ellipse()
+        //strokeWeight(event.pressure*50);
+        //line(pMouseX,pMouseY, mouseX, mouseY);
+    }
+    function handlePointerMove(event){
+        if(bCanPaint === true){
+            ellipse(mouseX,mouseY,brushRadius);
+        }
+    }
 
-    
+    function handlePointerUp(event){
+        bCanPaint = false;
+    }
+
 }
